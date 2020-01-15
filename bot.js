a728928e5950c6cb6f842b7df6a5de8e5607b637
adnangemthing <53823301+adnangemthing@users.noreply.github.com>
2020-01-15 13:39:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,15 +15,15 @@ client.on('message', msg => {  //WESO#0001
   });
 
 client.on('message', msg => {  //WESO#0001
-    if (msg.content === 'ويسو') {  //WESO#0001
-      msg.reply('**ويسو مات**');  //WESO#0001
+    if (msg.content === 'باي') {  //WESO#0001
+      msg.reply('**مع السلامة :heart_eyes: **');  //WESO#0001
     }
   });
 
 
 client.on('message', msg => {  //WESO#0001
-    if (msg.content === 'احبك') {  //WESO#0001
-      msg.reply('**لاتحتك يا اخوي لو سمحت**');  //WESO#0001
+    if (msg.content === 'كلب') {  //WESO#0001
+      msg.reply('**لاتعيدها لو سمحت**');  //WESO#0001
     }
   });
   
